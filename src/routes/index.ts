73d18dbb4f6266be28d2f99ca0c28d8949a65843
Christian Schliz <code@foxat.de>
2020-10-15 02:26:01 +0200

    Implemented health checking for mongodb and minio servers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,47 @@
 import { Router } from "express";
+import { db } from "../structs/database";
+import { minioConfig as getMinioConfig } from "../structs/config";
+import * as http from "http";
 
 const router = Router();
+const minioConfig = getMinioConfig();
 
 router.get("/", (req, res) => {
   res.end("Try /recipe");
+});
+
+router.get("/health", (req, res) => {
+  // Send back sattus code 200, the API is live.
+  res.status(200);
+
+  let minioHealthRequestOptions = {
+    method: "GET",
+    host: minioConfig.endPoint,
+    port: minioConfig.port,
+    path: "/minio/health/live",
+    timeout: 5000
+  };
+
+  // Check the minio server for health
+  const minioHealthRequest = http.request(minioHealthRequestOptions, (response) => {
+    res.send({
+      mongodb: db.readyState === 1,
+      minio: response.statusCode === 200
+    });
+    res.end();
+  });
+
+  minioHealthRequest.on("timeout", () => {
+    res.send({
+      mongodb: db.readyState === 1,
+      minio: false
+    });
+
+    // The minio server is either down or under heavy
+    // load, suggesting a restart might be needed.
+
+    res.end();
+  });
 });
 
 export default router;
